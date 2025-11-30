c908ef503b406a8f159f7c57f97c82970b2503c5
Kavya Malgi <86841818+kavyamalgi@users.noreply.github.com>
2025-11-30 11:01:17 -0600

    Update create_tables.sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,24 @@
+'''use cortex search when you want hybrid search (combination of vector embedding and keyword search)'''
+
+create or replace database BUILD_DEMO;
+use database BUILD_DEMO;
+create or replace schema BOOKS_DATA;
+use schema BOOKS_DATA;
+
+CREATE or replace TABLE GOODREADS_BOOKS
+(
+TITLE VARCHAR,
+AUTHOR VARCHAR,
+URL VARCHAR,
+STAR_RATING NUMBER
+)
+AS
+SELECT
+NAME,
+AUTHOR,
+URL,
+TO_NUMBER(STAR_RATING)
+FROM AI_TRAINING_DATASET_FROM_GOODREADS_BOOKS.PUBLIC.GOODREADSBOOK;
+
+SELECT * FROM GOODREADS_BOOKS;
 
